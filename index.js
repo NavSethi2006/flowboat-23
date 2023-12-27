37abbbfac0d4a7f6b538d4212afdd3f55cae5463
NavSethi2006 <navinsethi2006@gmail.com>
2023-12-26 19:38:35 -0500

    side bar after login made

---
+++ index.js
@@ -0,0 +1,7 @@
+function open_sidebar() {
+    document.getElementById("sidebar").style.display = "block";
+}
+  
+  function close_sidebar() {
+    document.getElementById("sidebar").style.display = "none";
+}
